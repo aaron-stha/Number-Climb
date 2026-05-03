187cdd01e09c9616b07d5e737eebf631d9081c8d
alexstha <krishshre1234@gmail.com>
2026-05-03 17:52:16 +0300

    Add difficulty selection and improve game logic

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import random
 
-numbers = list(range(1, 13))
+print("\n🎮 SELECT DIFFICULTY")
+print("1. Easy (1-9)")
+print("2. Medium (1-12)")
+print("3. Hard (1-15)")
 
+difficulty = input("> ")
+
+if difficulty == "1":
+    numbers = list(range(1, 10))
+
+elif difficulty == "2":
+    numbers = list(range(1, 13))
+
+elif difficulty == "3":
+    numbers = list(range(1, 16))
+
+else:
+    print("❌ Invalid choice! Defaulting to Medium.")
+    numbers = list(range(1, 13))
+best_score = 999
 
 def show_numbers(numbers):
     print(f"\nThe numbers are: {numbers}")
 
 
-def roll_dice():
-    dice1 = random.randint(1, 6)
-    dice2 = random.randint(1, 6)
-    return dice1, dice2, dice1 + dice2
+def roll_dice(numbers):
+    if max(numbers) <= 6:
+        dice1 = random.randint(1, 6)
+
+        print("rolling one dice only because you have less 7 numbers")
+        return dice1, 0, dice1
+    
+    else:
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+        return dice1, dice2, dice1 + dice2
 
 
 def remove_numbers(user_list, total, numbers):
@@ -22,6 +47,10 @@ def remove_numbers(user_list, total, numbers):
 
     if sum(user_list) != total:
         print("❌ Numbers do not match dice total")
+        return False
+    
+    if len(user_list) != len(set(user_list)):
+        print("Duplicate number is not allowed")
         return False
 
     for num in user_list:
@@ -55,8 +84,6 @@ def check_possible(total, numbers):
 
 def game():
 
-    numbers = list(range(1, 13))
-
     while True:
 
         if len(numbers) == 0:
@@ -67,14 +94,28 @@ def game():
 
         input("\nPress Enter to roll dice...")
 
-        dice1, dice2, total = roll_dice()
+        dice1, dice2, total = roll_dice(numbers)
 
-        print(f"\n🎲 You rolled {dice1} + {dice2} = {total}")
+        if dice2 == 0:
+            print(f"\n🎲 You rolled {dice1}")
+        else:
+            print(f"\n🎲 You rolled {dice1} + {dice2} = {total}")
 
         if not check_possible(total, numbers):
+
             print("\n💀 GAME OVER")
             print("Final numbers:", numbers)
+
+            score = sum(numbers)
             print("Score:", sum(numbers))
+
+            global best_score
+
+            if score < best_score:
+                best_score = score
+                print("new best score")
+
+            print(f"your best score was   {best_score}")
             break
 
         # PLAYER INPUT LOOP
@@ -106,7 +147,7 @@ while True:
     game()
 
     again = input("\nPlay again? (y/n): ")
-
-    if again != "y":
+    agains = again.upper()
+    if agains != "Y":
         print("👋 Thanks for playing!")
         break
